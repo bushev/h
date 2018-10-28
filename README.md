5c42abebf862e67bf0f1dfa916827a7c1979c07a
Yury Bushev <bushevuv@gmail.com>
2018-10-28 16:55:50 +0300

    Update deployment script.

---
+++ README.md
@@ -0,0 +1,30 @@
+# h
+
+> root@176.112.204.146
+> 3zM1KBEr
+
+# Install Docker
+https://docs.docker.com/install/linux/docker-ce/ubuntu/
+
+# Install docker-compose
+https://docs.docker.com/compose/install/#install-compose
+
+# Generate SSH key
+ssh-keygen -t rsa -b 4096 -C "bushevuv@gmail.com"
+
+# Copy SSH key
+ssh-copy-id -i ~/.ssh/h.pub root@176.112.204.146
+
+# Login SSH
+ssh root@176.112.204.146 -i ~/.ssh/h
+
+# Add host to .ssh/config
+
+# Prepare deployment
+
+# Install Node.js
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# Install pm2
+sudo npm i -g pm2 
---
+++ ecosystem.config.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+    apps: [{
+        name: 'Backend',
+        script: 'index.js',
+        env_production: {
+            NODE_ENV: 'production'
+        }
+    }],
+    deploy: {
+        production: {
+            key: '/Users/bushev/.ssh/h',
+            user: 'root',
+            host: '176.112.204.146',
+            repo: 'git@github.com:bushev/h.git',
+            path: '/root/h',
+            ref: 'origin/master',
+            'post-setup': 'pm2 startOrRestart ecosystem.config.js --env production',
+            'post-deploy': 'npm i',
+            env: {
+                NODE_ENV: 'production'
+            }
+        }
+    }
+};
