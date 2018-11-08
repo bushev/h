468f17ee68d2758091a64655b04f35586a5738ad
Kshevsky Dmitry <rudimo@yandex.ru>
2018-11-08 13:47:09 +0300

    Init ionic app

---
+++ ionic-app/src/pages/home/home.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import set = Reflect.set;
+// declare const Mazemap;
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+    // setTimeout(() => {
+    //   let myMap = new Mazemap.Map({container: 'mazemap-container'});
+    //
+    //   myMap.on('click', function(e){
+    //     myMap.flyTo({center:e.lngLat, zoom: 18});
+    //   });
+    // }, 1000);
+  }
+}
